98667444d72857817bb3bed18641db815f831430
Yang Huanhzhou <yanghuanzhou@sensetime.com>
2019-08-29 10:35:33 +0800

    更新答案

--- 19-获取文件的扩展名.jsx
+++ 19-获取文件的扩展名.jsx
@@ -3,5 +3,6 @@
 const extname = (filename) => {
 
   return filename.lastIndexOf('.') > 0 ? filename.slice(filename.lastIndexOf('.')) : '';
+  // return filename.lastIndexOf('.') > 0 ? "."+filename.split(".").pop() : '';
 }
 
